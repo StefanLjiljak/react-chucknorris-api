3c6231e72103c94003057469ee51a9042c467ae9
StefanLjiljak <stefanljiljak@gmail.com>
2021-08-29 17:14:49 +0200

    Updates

---
+++ precache-manifest.d0aa8ef0494a979aff2f30341ab54645.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a64bd0e31f8b56a555b134dc0c8ecb4d",
+    "url": "/react-chucknorris-api/index.html"
+  },
+  {
+    "revision": "5fd12817068aa236a503",
+    "url": "/react-chucknorris-api/static/css/main.b515e716.chunk.css"
+  },
+  {
+    "revision": "162f5353b77519a8f522",
+    "url": "/react-chucknorris-api/static/js/2.e8f22ea8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-chucknorris-api/static/js/2.e8f22ea8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fd12817068aa236a503",
+    "url": "/react-chucknorris-api/static/js/main.b159c8aa.chunk.js"
+  },
+  {
+    "revision": "085deeec4d793bcd5a6a",
+    "url": "/react-chucknorris-api/static/js/runtime-main.7af66772.js"
+  },
+  {
+    "revision": "39b94c0e41581c1258965096f71484c9",
+    "url": "/react-chucknorris-api/static/media/chuck.39b94c0e.jpg"
+  }
+]);
